5a316d0aedd4656f35fe18deb0fbbd1bc6685a75
Andreas Schaffner <andschaf@ethz.ch>
2015-05-26 17:11:12 +0200

    added ifndef, gps_rviz.h

--- gps_rviz/src/gps_handler.h
+++ gps_rviz/src/gps_handler.h
@@ -1,3 +1,6 @@
+#ifndef GPS_HANDLER_H_
+#define GPS_HANDLER_H_
+
 #include "geometry_msgs/Vector3.h"
 
 const float InitializingPoint_x =4266066; //TODO Define this point
@@ -8,4 +11,4 @@ const float majorAxis = 6378137; //in meters
 const float minorAxis 	= 6356752.31; //in meters
 
 
-
+#endif
--- gps_rviz/src/gps_rviz.cpp
+++ gps_rviz/src/gps_rviz.cpp
@@ -4,24 +4,30 @@
 #include "gps_handler.cpp"
 
 
+void publishGPS_Track(){
 
-void gps2rviz(const sensor_msgs::NavSatFix gps_NavSatFix){ //callback function
+	ros::NodeHandle n; //TODO necessary??
+	ros::Publisher gps_pub = n.advertise<visualization_msgs::MarkerArray>("GPS_marker",1000); //TODO rewrite this, nicht jedesmal advertisen
+	gps_pub.publish(gps::GPS_TRACK);
+	ros::spin();
+}
+
+void gps_callback(const sensor_msgs::NavSatFix gps_NavSatFix){ //callback function
 
 	geometry_msgs::Vector3 gpsOut;
 
 	gpsOut.x = gps_NavSatFix.latitude; 
 	gpsOut.y = gps_NavSatFix.longitude;
 	gpsOut.z = gps_NavSatFix.altitude;
-
-	ROS_INFO("I extracted: lat=[%f] , lon=[%f] and alt=[%f]" ,gpsOut.x, gpsOut.y, gpsOut.z);
 	
 	geometry_msgs::Vector3 cleaned_GPS_point;
 	cleaned_GPS_point = gps::gps2cart(gpsOut);	
 
 	gps::addToTrack(cleaned_GPS_point);	
 	
-	gps::publishGPS_Track();
+	publishGPS_Track(); 
 } 
+
 
 
 int main(int argc, char **argv)
@@ -33,7 +39,7 @@ int main(int argc, char **argv)
 
 	gps::initializeTrack();
 	
-	ros::Subscriber sub = n.subscribe("/gps/fix" , 1000, gps2rviz);
+	ros::Subscriber sub = n.subscribe("/gps/fix" , 1000, gps_callback);
 
 	ros::spin();
 	return 0;

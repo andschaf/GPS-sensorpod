8de81ca72a3951b21c451c56c89ce9babde0b55a
Andreas Schaffner <andschaf@ethz.ch>
2015-05-27 13:28:34 +0200

    Added bagfile for testing, see HowToRunIt for instructions; Restructured, cleaned up

--- gps_rviz/src/gps_handler.cpp
+++ gps_rviz/src/gps_handler.cpp
@@ -10,22 +10,46 @@ using namespace std;
 
 namespace gps{
 
-visualization_msgs::MarkerArray GPS_TRACK;
-visualization_msgs::Marker gps_points, gps_line_strip, gps_line_list;
+ geometry_msgs::Vector3 InitializingPoint, InitializingPoint_missionframe;
+ bool REFERENCE_FRAME_IS_INITIALIZED;
+ float alpha_init, beta_init;
+ 
+ visualization_msgs::MarkerArray GPS_TRACK;
+ visualization_msgs::Marker gps_points, gps_line_strip, gps_line_list;
+
+// ---------------------------------------------
+// Rotational Matrix R_0G=R_01*R_1G; R_01=Rz(a); R_1G=Ry(b)
+geometry_msgs::Vector3 transformation(geometry_msgs::Vector3 Input, float a, float b){
+	geometry_msgs::Vector3 Output;
+ 	Output.x = cos(a)*cos(b)*Input.x - sin(a)*Input.y +cos(a)*sin(b)*Input.z;
+ 	Output.y = sin(a)*cos(b)*Input.x + cos(a)*Input.y +sin(a)*sin(b)*Input.z;
+ 	Output.z = -sin(b)*Input.x + cos(b)*Input.z;
+
+return Output;
+}
+
+// -------------------------------------------------
+// Rotational Matrix plus shifting
+geometry_msgs::Vector3 transformToMissionFrame(geometry_msgs::Vector3 InputPoint){
+
+	geometry_msgs::Vector3 transformed_point;
+
+	transformed_point = transformation(InputPoint, alpha_init, beta_init); //Rotate by alpha, beta
+	transformed_point.x = transformed_point.x - InitializingPoint_missionframe.x; // shift to normalize
+	transformed_point.y = transformed_point.y - InitializingPoint_missionframe.y;
+	transformed_point.z = transformed_point.z - InitializingPoint_missionframe.z;
+
+return transformed_point;
+} 
+
+// ---------------------------------------------------
+// NavSatFix coordinates to WGS84 (cartesian) coordinates
+geometry_msgs::Vector3 gps2cart(const geometry_msgs::Vector3& gpsOut){
 	
-geometry_msgs::Vector3 gps2cart(const geometry_msgs::Vector3& gpsOut)
-{	
-	float e; 
-	float R;
+	float e, R, height, phi_deg, phi_rad, lambda_deg, lambda_rad; 
 
 	R = (majorAxis+minorAxis)/2; //in meters
 	e = (majorAxis*majorAxis-minorAxis*minorAxis)/(majorAxis*majorAxis); //in meters
-
-	float height; 		//meters altitude
-	float phi_deg; 		//degree, latitude
-	float lambda_deg; 	//degree, longitude
-	float phi_rad;
-	float lambda_rad;
 
 	height 		= gpsOut.z; 		//meters
 	phi_deg		= gpsOut.x; 		//degrees
@@ -34,7 +58,7 @@ geometry_msgs::Vector3 gps2cart(const geometry_msgs::Vector3& gpsOut)
 	lambda_rad 	= lambda_deg/180*PI; 	//radians
 
 	geometry_msgs::Vector3 gps_cart;
-	gps_cart.x = (R+height)*cos(phi_rad)*cos(lambda_rad); //TODO introduce WGS84!!
+	gps_cart.x = (R+height)*cos(phi_rad)*cos(lambda_rad); // WGS84
 	gps_cart.y = (R+height)*cos(phi_rad)*sin(lambda_rad);
 	gps_cart.z = (R*(1-e*e)+height)*sin(phi_rad);
 
@@ -42,13 +66,14 @@ geometry_msgs::Vector3 gps2cart(const geometry_msgs::Vector3& gpsOut)
 return gps_cart;
 }
 
-
+// ----------------------------------------------
+// Add a single point (x,y,z) to MarkerArray GPS_TRACK 
 void addToTrack(const geometry_msgs::Vector3 PointToBeAddedToTrack){
 
 	geometry_msgs::Point p;
-	p.x = PointToBeAddedToTrack.x-InitializingPoint_x;
-	p.y = PointToBeAddedToTrack.y-InitializingPoint_y;
-	p.z = PointToBeAddedToTrack.z-InitializingPoint_z;
+	p.x = PointToBeAddedToTrack.x;
+	p.y = PointToBeAddedToTrack.y;
+	p.z = PointToBeAddedToTrack.z;
 
 	gps_points.points.push_back(p);
 	gps_line_strip.points.push_back(p);
@@ -59,11 +84,12 @@ void addToTrack(const geometry_msgs::Vector3 PointToBeAddedToTrack){
 	GPS_TRACK.markers.push_back(gps_points);
 	GPS_TRACK.markers.push_back(gps_line_strip);
 	GPS_TRACK.markers.push_back(gps_line_list);
+	ROS_INFO("Adding point [%f],[%f], [%f] in mission frame to Track.", PointToBeAddedToTrack.x,PointToBeAddedToTrack.y,PointToBeAddedToTrack.z); 	
 }
 
+// ------------------------------------------------------------
+// Define parameters of GPS_TRACK
 void initializeTrack(){
-
-// Define all the parameters
 	gps_points.header.frame_id = gps_line_strip.header.frame_id = gps_line_list.header.frame_id = "map"; 
 	gps_points.header.stamp = gps_line_strip.header.stamp = gps_line_list.header.stamp = ros::Time::now();
 	gps_points.ns = gps_line_strip.ns =gps_line_list.ns = "gps_points_and_lines";
@@ -90,15 +116,7 @@ void initializeTrack(){
 	gps_line_list.color.a = 1.0;
 
 	ROS_INFO("Defined type, scale and color successfully");
+}
 
-	geometry_msgs::Vector3 InitializingPoint;
-	InitializingPoint.x = InitializingPoint_x;
-	InitializingPoint.y = InitializingPoint_y;
-	InitializingPoint.z = InitializingPoint_z;
-	
-	addToTrack(InitializingPoint);
-	ROS_INFO("Initialized Track successfully. First point: [%f],[%f],[%f].", InitializingPoint.x, InitializingPoint.y, InitializingPoint.z);
-
-} //end initializeTrack()
-
+// ----------------------------------------------------------------------
 } //end namespace gps
--- gps_rviz/src/gps_rviz.cpp
+++ gps_rviz/src/gps_rviz.cpp
@@ -2,12 +2,14 @@
 #include "sensor_msgs/NavSatFix.h"
 #include "geometry_msgs/Vector3.h"
 #include "gps_handler.cpp"
+#include "gps_rviz.h"
+#define PI 3.14159265
 
 
 void publishGPS_Track(){
 
-	ros::NodeHandle n; //TODO necessary??
-	ros::Publisher gps_pub = n.advertise<visualization_msgs::MarkerArray>("GPS_marker",1000); //TODO rewrite this, nicht jedesmal advertisen
+	ros::NodeHandle node_handle_; 
+	ros::Publisher gps_pub = node_handle_.advertise<visualization_msgs::MarkerArray>("GPS_marker",1000);
 	gps_pub.publish(gps::GPS_TRACK);
 	ros::spin();
 }
@@ -16,31 +18,46 @@ void gps_callback(const sensor_msgs::NavSatFix gps_NavSatFix){ //callback functi
 
 	geometry_msgs::Vector3 gpsOut;
 
-	gpsOut.x = gps_NavSatFix.latitude; 
-	gpsOut.y = gps_NavSatFix.longitude;
+	gpsOut.x = gps_NavSatFix.latitude; // TODO this is sort of a hack...
+	gpsOut.y = gps_NavSatFix.longitude; // x,y,z is misleading
 	gpsOut.z = gps_NavSatFix.altitude;
 	
-	geometry_msgs::Vector3 cleaned_GPS_point;
-	cleaned_GPS_point = gps::gps2cart(gpsOut);	
+	geometry_msgs::Vector3 GPS_point_WGS84;
+	GPS_point_WGS84 = gps::gps2cart(gpsOut);	
 
-	gps::addToTrack(cleaned_GPS_point);	
+	if (!gps::REFERENCE_FRAME_IS_INITIALIZED){
+	 ROS_INFO("Initializing Reference Frame...");
+	 gps::InitializingPoint = GPS_point_WGS84;
+	 gps::alpha_init = PI + gps_NavSatFix.longitude/180*PI;
+	 gps::beta_init = PI/2 - gps_NavSatFix.latitude/180*PI;
+	 gps::REFERENCE_FRAME_IS_INITIALIZED = true;
 	
+	 gps::InitializingPoint_missionframe = gps::transformation(gps::InitializingPoint, gps::alpha_init, gps::beta_init);
+	 
+	} //endif
+
+	geometry_msgs::Vector3 GPS_point_missionframe;
+	GPS_point_missionframe = gps::transformToMissionFrame(GPS_point_WGS84); 
+	gps::addToTrack(GPS_point_missionframe);	
 	publishGPS_Track(); 
 } 
 
-
-
 int main(int argc, char **argv)
 {	
+
+
 	ROS_INFO("You are running playground!!!");
 	ros::init(argc, argv, "nGPSlistener");
+	ros::NodeHandle node_handle_;
 	
-	ros::NodeHandle n;
-
 	gps::initializeTrack();
-	
-	ros::Subscriber sub = n.subscribe("/gps/fix" , 1000, gps_callback);
+
+	ros::Subscriber sub = node_handle_.subscribe("/gps/fix" , 1000, gps_callback);
 
 	ros::spin();
 	return 0;
 }
+
+
+// TODO real data set, validate, check with google maps
+

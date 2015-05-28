cefd7fa3e68fa225ae5a011659765776ac1cc14d
Andreas Schaffner <andschaf@ethz.ch>
2015-05-28 17:22:03 +0200

    changed float precision to double. Introduced proper WGS84 convention. Introduced proper Initialization.

--- gps_rviz/src/gps_handler.h
+++ gps_rviz/src/gps_handler.h
@@ -3,9 +3,9 @@
 
 #include "geometry_msgs/Vector3.h"
 
-const float kMajorAxis = 6378137; 
-const float kMinorAxis 	= 6356752.31;
-const float kEarthRadius = (kMajorAxis+kMinorAxis)/2.0; 
-const float kEccentricity = (kMajorAxis*kMajorAxis-kMinorAxis*kMinorAxis)/(kMajorAxis*kMajorAxis); 
+const double kMajorAxis = 6378137; 
+const double kMinorAxis	= 6356752.31;
+const double kEarthRadius = (kMajorAxis+kMinorAxis)/2.0;  //TODO(andschaf): probably remove this.
+const double kEccentricity = (kMajorAxis*kMajorAxis-kMinorAxis*kMinorAxis)/(kMajorAxis*kMajorAxis); 
 
 #endif
